291f31bc0ee10f18b82af8b07e0ba0d32460eb02
Brice404 <Amenle.Foumane@outlook.com>
2026-04-03 13:56:04 -0400

    In the process of changing conversion into the more readable de morgan's law. IDK what I'm doing pls send help 😭🙏

--- Node.java
+++ Node.java
@@ -21,70 +21,103 @@ class Node {
 
     Node toNAND(){
         switch (type) {
-            case VARIABLE:
+            case VARIABLE -> {
                 return new Node(value);
+            }
 
-            case NOT: {
+            case NOT -> {
                 Node a = left.toNAND();
                 return new Node(Symbol.NAND, a, a);
             }
 
-            case AND: {
+            case AND -> {
                 Node a = left.toNAND();
                 Node b = right.toNAND();
                 Node t = new Node(Symbol.NAND, a, b);
                 return new Node(Symbol.NAND, t, t);
             }
 
-            case OR: {
+            case OR -> {
                 Node a = left.toNAND();
                 Node b = right.toNAND();
                 return new Node(Symbol.NAND, new Node(Symbol.NAND, a, a), new Node(Symbol.NAND, b, b));
             }
 
-            default:
-                throw new RuntimeException("Cannot convert " + type + " to NAND");
+            case NAND -> {
+                Node a = left.toNAND();
+                Node b = right.toNAND();
+                return new Node(Symbol.NAND, a, b);
+            }
+
+            case NOR -> {
+                Node a = left.toNAND();
+                Node b = right.toNAND();
+                Node orNode = new Node(Symbol.NAND, new Node(Symbol.NAND, a, a), new Node(Symbol.NAND, b, b));
+                return new Node(Symbol.NAND, orNode, orNode);
+            }
+
+            default -> throw new RuntimeException("Cannot convert " + type + " to NAND");
         }
     }
 
     Node toNOR(){
         switch (type) {
-            case VARIABLE:
+            case VARIABLE -> {
                 return new Node(value);
+            }
 
-            case NOT: {
+            case NOT -> {
                 Node a = left.toNOR();
                 return new Node(Symbol.NOR, a, a);
             }
 
-            case AND: {
+            case AND -> {
                 Node a = left.toNOR();
                 Node b = right.toNOR();
                 return new Node(Symbol.NOR, new Node(Symbol.NOR, a, a), new Node(Symbol.NOR, b, b));
             }
 
-            case OR: {
+            case OR -> {
                 Node a = left.toNOR();
                 Node b = right.toNOR();
                 Node t = new Node(Symbol.NOR, a, b);
                 return new Node(Symbol.NOR, t, t);
             }
 
-            default:
-                throw new RuntimeException("Cannot convert " + type + " to NOR");
+             case NOR -> {
+                Node a = left.toNOR();
+                Node b = right.toNOR();
+                return new Node(Symbol.NOR, a, b);
+            }
+ 
+            case NAND -> {
+                Node a = left.toNOR();
+                Node b = right.toNOR();
+                Node andNode = new Node(Symbol.NOR, new Node(Symbol.NOR, a, a), new Node(Symbol.NOR, b, b));
+                return new Node(Symbol.NOR, andNode, andNode);
+            }
+
+            default -> throw new RuntimeException("Cannot convert " + type + " to NOR");
         }
+    }
+
+    private String negated(Node inner) {
+        if (inner.type == Symbol.VARIABLE) {
+            return "¬" + inner;
+        }
+        return "¬(" + inner + ")";
     }
 
     @Override
     public String toString() {
-        if (type == Symbol.VARIABLE) {
-            return value;
-        }
-
-        if (type == Symbol.NOT) {
-            return "(" + type + " " + left + ")";
-        }
-
-        return "(" + left + " " + type + " " + right + ")";
+        return switch (type) {
+            case VARIABLE -> value;
+            case NOT -> negated(left);
+            case AND -> "(" + left + " ∧ " + right + ")";
+            case OR -> "(" + left + " ∨ " + right + ")";
+            case NAND -> left == right ? negated(left) : "¬(" + left + " * " + right + ")";
+            case NOR -> left == right ? negated(left) : "¬(" + left + " + " + right + ")";
+            default -> "(" + left + " " + type + " " + right + ")";
+        };
     }
 }
